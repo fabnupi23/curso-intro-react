3de5a7adf6d37d37045077b653193e5ede5402ee
fabnupi23 <31360808+fabnupi23@users.noreply.github.com>
2022-09-04 21:37:03 -0500

    pruebas

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+      <div className='card-body'>
+          <h1 className='card-title'>Mi titulo</h1>
+          <p className='card-text'>haaahahahahahahahahahahah</p>
+      </div>
+    </div>
+  )
+}
+
+export { Card };
